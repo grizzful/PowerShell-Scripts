3a503371639579ff8f6434a10f53fd41b7dcf5ab
grizzful <150555435+grizzful@users.noreply.github.com>
2025-04-08 19:32:10 -0400

    Create Remove User From Groups.ps1

---
+++ Remove User From Groups.ps1
@@ -0,0 +1,25 @@
+$OUPath = "OU=Inactive Accounts,OU=ACT Learning Centre,DC=ottawa,DC=communicare,DC=int"
+$ExportPath = "C:\CDITS\RemoveFromGroups.csv"
+
+Get-ADUser -Filter * -SearchBase $OUpath | Select-object DistinguishedName,Name,UserPrincipalName | Export-Csv -NoType $ExportPath
+
+$import = Import-Csv $ExportPath
+
+ForEach ($user in $import) {
+
+    # Removes user from all groups expect domain_users
+
+    Write-Output `n"Removing $import From all AD Groups..."
+
+    try{
+
+        Get-AdPrincipalGroupMembership -Identity $import | Where-Object -Property Name -Ne -Value 'Domain Users' | Remove-AdGroupMember -Members $import
+
+        Write-Host "Removed $import Group Membership" -Foregroundcolor Cyan
+
+    }catch{
+        Write-Host "Failed to Remove Group Membership!" -Foregroundcolor Red
+    }
+}
+
+Write-Host "Done!"
